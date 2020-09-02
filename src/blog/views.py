25bff9936c284869a98e1d7ffac517b99db158f1
williamboco21 <williammboco@gmail.com>
2020-09-02 11:13:43 +0800

    users and models

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'William',
-        'title': 'Blog Post 1',
-        'content': 'This is my first post content',
-        'date_posted': 'September 1, 2020'
-    },
-    {
-        'author': 'django',
-        'title': 'Blog Post 2',
-        'content': 'This is my second post content',
-        'date_posted': 'August 31, 2020'
-    },
-]
+# from the current package from the root, import the class
+from .models import Post
 
 # Create your views here.
 
 
 def home_view(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/index.html', context)
 
