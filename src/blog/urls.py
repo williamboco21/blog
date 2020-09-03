5acb1284e9453664eab373b4ade6a30370e517e7
williamboco21 <williammboco@gmail.com>
2020-09-03 13:31:16 +0800

    create, update and delete functions

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import PostsListView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', PostsListView.as_view(), name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about_view, name="blog-about"),
 ]
